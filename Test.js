5fedc62d158f18a8a3fb7dda516355e2b3ae0240
Hong29072005 <mrhong563@gmail.com>
2025-01-19 19:03:09 +0700

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,12 @@
+
+function menu_bar(){
+   document.getElementById('ul').style.marginLeft='0%'
+   
+}
+function menu_bars(){
+    document.getElementById('ul_bar').style.marginLeft='-100%'
+    
+ }
+// function iconbar(){
+//    document.getElementById('icon_bar').style.display='none'
+// }
